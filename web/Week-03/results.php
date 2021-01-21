7ef60ad2ad58d7120a5d829d509dae183d420a70
Lava84flow <Lava84flow@users.noreply.github.com>
2021-01-20 18:53:24 -0800

    Update results.php
    could not fix continents on heroku but they work locally

--- web/Week-03/results.php
+++ web/Week-03/results.php
@@ -5,6 +5,24 @@
 <title>Form Results | 03</title>
 <link href="team03.css" rel="stylesheet" type="text/css">
 </head>
+  
+    
+    <?php
+        // First let's process all the input
+
+        // using constants for the names of the elements in the form would be better...
+
+        // It would also be better to use an ID of some sort for the
+        // value that is submitted such as "cs" as opposed to "Computer Science",
+        // then in PHP we could process that value and determine the exact
+        // presentation text to render.
+//        $name = htmlspecialchars($_POST["name"]);
+//        $email = htmlspecialchars($_POST["email"]);
+//        $major = htmlspecialchars($_POST["major"]);
+//        $places = $_POST["places"];
+//        $comments = htmlspecialchars($_POST["comments"]);
+
+    ?>
     
     <span>Welcome <?php echo $_POST["name"]; ?></span><br>
     
@@ -19,7 +37,7 @@
     <br>
     
     
-    <
+    <span>We don't know why this part doesn't work on heroku but it works locally</span><br>
     <span>You Visited: 
         
         <?php
@@ -30,7 +48,7 @@
                 "AS" => "Asia",
                 "AU" => "Australia",
                 "AF" => "Africa",
-                "AN" => "Antarctica",
+                "AN" => "Antarctica"
             );
         
         
