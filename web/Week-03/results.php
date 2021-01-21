a40a9d500ca00787f7c384eaa1028bd6deb789e1
Lava84flow <Lava84flow@users.noreply.github.com>
2021-01-20 17:47:21 -0800

    step 02 done
    finished step 02 of the activity

--- web/Week-03/results.php
+++ web/Week-03/results.php
@@ -2,9 +2,23 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>Form Results | 03</title>
 <link href="team03.css" rel="stylesheet" type="text/css">
 </head>
+    
+    <span>Welcome <?php echo $_POST["name"]; ?></span><br>
+    
+    <br>
+    
+    <span>Your email address is: <a href="mailto:<?php echo $_POST["email"]; ?>"><?php echo $_POST["email"]; ?></a></span><br>
+    
+    <br>
+    
+    <span>Your Major is: <?php echo $_POST["major"]; ?></span><br>
+    
+    <br>
+    
+    <span>Your comment was: <br/><?php echo $_POST["comments"]; ?> </span><br>
 
 <body>
 </body>
