88635bbbd21f899b675146ca1bd602547ff2d4dd
Lava84flow <Lava84flow@users.noreply.github.com>
2021-01-21 17:26:17 -0800

    started basic shopping cart
    got the main shopping cart page mostly finished

--- web/Week-03/store.php
+++ web/Week-03/store.php
@@ -1,3 +1,30 @@
+<?php
+// Start the session
+session_start();
+
+
+$shopping_cart = [];
+
+if (isset($_POST['AddCart'])) {
+
+    switch ($_POST['AddCart']) {
+        case "bull":
+            echo "You pressed Button 1";
+            break;
+        case "cliffs":
+            echo "You pressed Button 2";
+            break;
+        case "orc":
+            echo "You pressed Button 3";
+            break;
+        case "space":
+            echo "You pressed Button 4";
+            break;
+        }
+    }
+
+?>
+
 <!doctype html>
 <html>
 <head>
@@ -29,6 +56,55 @@
     
     <main>
         
+        <form action="" method="post" class="store">
+            
+        <div class="store-item">
+            
+            <img class="thumb" src="img/f05c82111541689.60041b9f63641-thumb.png">
+            <div class="centered-button">
+                <button type="submit" name="AddCart" value="bull">Add To Cart</button>
+            </div>
+        </div>
+        
+        <div class="store-item">
+            
+            <img class="thumb" src="img/jbCNvTM4gwr2qV8X8fW3ZB-970-80-thumb.png">
+            
+            <div class="centered-button">
+                <button type="submit" name="AddCart" value="cliffs">Add To Cart</button>
+            </div>
+            
+        </div>
+        
+        <div class="store-item">
+            
+            <img class="thumb" src="img/orc07-thumb.png">
+            
+            <div class="centered-button">
+                <button type="submit" name="AddCart" value="orc">Add To Cart</button>
+            </div>
+            
+        </div>
+        
+        <div class="store-item">
+            
+            <img class="thumb" src="img/pixelart_P1_900x420-thumb.png">
+            
+            <div class="centered-button">
+                <button type="submit" name="AddCart" value="space">Add To Cart</button>
+            </div>
+            
+        </div>
+            
+            <div style="text-align: center">
+                
+            </div>
+        </form>
+        <form action="shoppingcart.php" method="post" class="submit">
+            <div style="text-align: center">
+                <button type="submit" value="View Cart">View Cart</button>
+            </div>
+        </form>
     </main>
     
     <div>
