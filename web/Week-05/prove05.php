d85d101e5257800c197f2ff0121306fbcd06a1ce
Lava84flow <Lava84flow@users.noreply.github.com>
2021-02-01 16:06:06 -0800

    added skeleton for Week 05 pages

--- web/Week-05/prove05.php
+++ web/Week-05/prove05.php
@@ -2,9 +2,41 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>Untitled</title>
+    
+    <link href="../shared/main.css" rel="stylesheet" type="text/css">
+    <script src="../shared/main.js" defer></script>
+    <script src="shoppingcart.js" defer></script>
 </head>
 
 <body>
+    
+        <div class="nav-bar sticky clearfix">
+    <nav>
+        <?php include '../shared/nav.php'; ?>  
+    </nav>
+    </div>
+    
+    <div id="wrapper">
+        
+    <div>
+    <header>        
+        <?php 
+            include '../shared/header.php';
+            echo '<h3>Week 05 | Untitled</h3></span>';
+        ?>    
+    </header>
+    </div>
+    
+    <main>
+        
+    </main>
+    <div>
+    <footer class="clearfix">
+        <?php include '../shared/footer.php'; ?>
+    </footer>
+    </div>
+    </div>
+    
 </body>
 </html>
