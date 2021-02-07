591dc633bff196126dcd4d369b62451cceba357b
Lava84flow <Lava84flow@users.noreply.github.com>
2021-02-06 18:42:56 -0800

    added orderresutls page

--- web/Week-05/orderresults.php
+++ web/Week-05/orderresults.php
@@ -1,20 +1,24 @@
 <?php
-/*
+
 require '../shared/dbconnect.php';
+
+$fname = htmlspecialchars($_POST["fname"]);
+
+
 $db = get_db();
-*/
+
 /*
 $query1 = 'SELECT * FROM anniesattic.customers';
 $stmt = $db->prepare($query1);
 $stmt->execute();
 $customers = $stmt->fetchAll(PDO::FETCH_ASSOC);
 */
-/*
-$query2 = 'SELECT * FROM anniesattic.orders o JOIN anniesattic.customers c ON o.customers_idcustomers = c.idcustomers WHERE c.first_name = \'Mary\'';
-$stmt = $db->prepare($query2);
+
+$query = 'SELECT * FROM anniesattic.orders o JOIN anniesattic.customers c ON o.customers_idcustomers = c.idcustomers WHERE c.first_name = \''.$fname.'\'';
+$stmt = $db->prepare($query);
 $stmt->execute();
 $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
-*/
+
 
 ?>
 
@@ -22,7 +26,7 @@ $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <html>
 <head>
 <meta charset="utf-8">
-<title>PHP Data Access</title>
+<title>Orders</title>
     
     <link href="../shared/main.css" rel="stylesheet" type="text/css">
     <script src="../shared/main.js" defer></script>
@@ -42,33 +46,14 @@ $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <header>        
         <?php 
             include '../shared/header.php';
-            echo '<h3>Week 05 | PHP Data Access</h3></span>';
+            echo '<h3>Week 05 | Orders</h3></span>';
         ?>    
     </header>
     </div>
     
     <main>
-        <p>
-            <?php
-/*
-            foreach ($customers as $customer)
-                {
-                    $id = $customer['idcustomers'];
-                    $fname = $customer['first_name'];
-                    $lname = $customer['last_name'];
-                    $uname = $customer['username'];
-                    $email = $customer['email'];
-                    $password = $customer['password'];
+        <?php
 
-
-                    echo "<p>$id, $fname, $lname, $uname, $email, $password</p>";
-                }
-*/
-            ?>
-        </p>
-        <p>
-            <?php
-/*
             foreach ($orders as $order)
                 {
                     $id = $order['idorders'];
@@ -83,20 +68,8 @@ $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                     echo "<p>$id, $userid, $subtotal, $taxes, $shipping, $status, $shipping, $billing</p>";
                 }
-*/
+
             ?>
-        </p>
-        
-        <div>
-            <p>
-                Search for orders from a customer. Your choices are "Mary" or "John" right now.
-            </p>
-            
-        <form action="orderresults.php" method="post">
-            First Name: <input type="text" name="fname">
-            <input type=submit value="Search">
-        </form>
-        </div>
     </main>
         
     <div>
