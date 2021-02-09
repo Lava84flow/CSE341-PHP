5dd3adddb1dbd4be45af769b8552dde715ba325a
Lava84flow <Lava84flow@users.noreply.github.com>
2021-02-09 11:01:08 -0800

    Create prove06.php
    added Prove06 page and skeleton

---
+++ web/Week-06/prove06.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP Data Update</title> 
+<link href="../shared/main.css" rel="stylesheet" type="text/css">
+<script src="../shared/main.js" defer></script>
+</head>
+
+<body>
+    
+    <div class="nav-bar sticky clearfix">
+    <nav>
+        <?php include '../shared/nav.php'; ?>  
+    </nav>
+    </div>
+    
+    <div id="wrapper">
+        
+    <div>
+    <header>        
+        <?php 
+            include '../shared/header.php';
+            echo '<h3>Week 05 | PHP Data Access</h3></span>';
+        ?>    
+    </header>
+    </div>
+    
+    <main>
+        
+    </main>
+        
+    <div>
+    <footer class="clearfix">
+        <?php include '../shared/footer.php'; ?>
+    </footer>
+    </div>
+    </div>
+    
+</body>
+</html>
