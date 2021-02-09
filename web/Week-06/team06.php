2f9685d0a3c7d3e84b45f083d0e118dc29f87120
Lava84flow <Lava84flow@users.noreply.github.com>
2021-02-09 11:17:41 -0800

    Create team06.php
    added week 06 team page and skeleton

---
+++ web/Week-06/team06.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Team activity</title> 
+<link href="../shared/main.css" rel="stylesheet" type="text/css">
+<script src="../shared/main.js" defer></script>
+</head>
+
+<body>
+    
+    <div class="nav-bar sticky clearfix">
+    <nav>
+        <?php include '../shared/nav.php'; ?>  
+    </nav>
+    </div>
+    
+    <div id="wrapper">
+        
+    <div>
+    <header>
+        <?php
+            include '../shared/header.php';
+            echo '<h3>Week 06 | Team activity</h3></span>';
+        ?>
+    </header>
+    </div>
+
+    <main>
+        
+    </main>
+
+    <div>
+    <footer class="clearfix">
+        <?php include '../shared/footer.php'; ?>
+    </footer>
+    </div>
+    </div>
+
+</body>
+</html>
