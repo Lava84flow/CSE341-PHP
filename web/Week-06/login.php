2680cb40000d52ae1a55574b274ae237a9b1a7d4
Lava84flow <Lava84flow@users.noreply.github.com>
2021-02-13 10:24:39 -0800

    Update login.php

--- web/Week-06/login.php
+++ web/Week-06/login.php
@@ -69,7 +69,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["fname"] = $first_name;
                             $_SESSION["lname"] = $last_name;
                             
-                            echo var_dump($_SESSION[]);
+                            //echo var_dump($_SESSION[]);
                             
                             // Redirect user to welcome page
                             header("location: welcome.php");
