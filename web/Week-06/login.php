d4f1b70c4de9e07573ff99ff0b2bc904b5abe338
Lava84flow <Lava84flow@users.noreply.github.com>
2021-02-13 10:01:14 -0800

    Update login.php

--- web/Week-06/login.php
+++ web/Week-06/login.php
@@ -34,11 +34,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     
     if(empty($username_err) && empty($password_err)){
-        $query = 'SELECT idcustomers, first_name, last_name, username, password FROM anniesattic.customers WHERE username = :username';
+        $query = 'SELECT idcustomers, first_name, last_name, username, password FROM anniesattic.customers WHERE username = :username;';
         $stmt = $db->prepare($query);
         
         $param_username = $username;
         $stmt->bindValue(':username', $param_username, PDO::PARAM_STR);
+        
+        var_dump($stmt);
         
         $stmt->execute();
         $scripture = $stmt->fetchAll(PDO::FETCH_ASSOC);
