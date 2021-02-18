4ba06ac38983f5c01aa26a2044f0a1fdf0df80b6
Lava84flow <Lava84flow@users.noreply.github.com>
2021-02-17 16:58:50 -0800

    Update welcome.php

--- web/Week-07/welcome.php
+++ web/Week-07/welcome.php
@@ -1,3 +1,4 @@
+<div style="width: 100%; margin: auto;" >
 <div class="page-header">
     <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["fname"]); ?> <?php echo htmlspecialchars($_SESSION["lname"]); ?></b>. Welcome to our site.</h1>
     <h2>Your username is: <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b></h2>
@@ -6,3 +7,4 @@
     <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
     <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
 </p>
+</div>
