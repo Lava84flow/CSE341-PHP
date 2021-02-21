704a175a92dd08fd0edf3687af503eff4de98b3d
Lava84flow <Lava84flow@users.noreply.github.com>
2021-02-20 19:22:23 -0800

    Update shopping-cart.php

--- web/Week-07/shopping-cart.php
+++ web/Week-07/shopping-cart.php
@@ -5,6 +5,8 @@ session_start();
 require '../shared/dbconnect.php';
 $db = get_db();
 
+$price_total = [];
+
 function getproduct($id) {
     
     $db = get_db();
@@ -40,7 +42,7 @@ if (isset($_POST['RemoveCart'])) {
     $_SESSION["shopping_cart"] = $shopping_cart;
 }
 
-var_dump($shopping_cart)
+//var_dump($shopping_cart)
 
 ?>
 
@@ -108,26 +110,27 @@ var_dump($shopping_cart)
                 
                 $cart_item = getproduct($id);
                     
-                var_dump($cart_item);
+                //var_dump($cart_item);
                 
-                var_dump($cart_item["img_url"]);
+                //var_dump($cart_item["img_url"]);
                     
                 echo    '<div class="store-item"><img class="thumb" src="'.$cart_item["img_url"].'">'.
                     '<div class="centered-button">
                         <button type="submit" name="RemoveCart" value="'.key($shopping_cart).'">Delete From Cart</button>
                     </div></div>';
                     next($shopping_cart);
+                
+                    array_push($price_total, $cart_item["price"]);
                 //<img class="thumb" src="img/pixelart_P1_900x420-thumb.png">
             }
         }
-        
-            
             
         ?>
         
-            <span><strong>Subtotal: $<?php echo number_format((float)array_sum($price), 2, '.', ''); ?></strong></span>
+            <span><strong>Subtotal: $<?php echo number_format((float)array_sum($price_total), 2, '.', ''); ?></strong></span>
             
         </form>
+        
         
             
         <div class="centered-button" style="padding: 1em">
