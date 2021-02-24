7af8bca1da94ee660fe68a5572f232946f77c01d
Lava84flow <Lava84flow@users.noreply.github.com>
2021-02-24 14:27:22 -0800

    Update HelloWorld.js
    finished hello world

--- web/Week-08/HelloWorld.js
+++ web/Week-08/HelloWorld.js
@@ -6,17 +6,36 @@ var http = require('http');
 function sayHello(reg, res) {
     console.log('Recived a reguest for: ' + reg.url);
 
-    res.write('<h1>Hello from Node</h1>');
-    res.end();
+    if (reg.url == '/') {
+
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write('<h1>Welcome to the homepage</h1>');
+        res.end();
+        
+    } else if (reg.url == '/home') {
+
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write('<h1>Hello World!</h1>');
+        res.end();
+
+    } else if (reg.url == '/getData') {
+        
+        res.writeHead(200, {"Content-Type": "application/json"});
+        
+        var data = {name: "Bryce Dickson", class: "CSE-341"}
+        
+        res.write(JSON.stringify(data));
+        res.end();
+
+    } else {
+
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.write('<h1>Error 404 page not found</h1>');
+        res.end();
+
+    }
+
 }
-
-
-
-
-
-
-
-
 
 var server = http.createServer(sayHello);
 
